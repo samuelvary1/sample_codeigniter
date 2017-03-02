9d97365953713b1f94535d97ff7453bc30bfb9c3
Sam Vary <sam.vary@gmail.com>
2017-03-02 12:25:15 -0500

    oh i see

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,7 +1,6 @@
 <html>
-        <head>
-                <title>CodeIgniter Tutorial</title>
-        </head>
-        <body>
-
-                <h1><?php echo $title; ?></h1>
+  <head>
+    <title>CodeIgniter Tutorial</title>
+  </head>
+  <body>
+    <h1><?php echo $title; ?></h1>
